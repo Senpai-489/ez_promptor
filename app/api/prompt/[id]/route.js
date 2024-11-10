e3fefeed6a04b09e1e5b9cbd3624750b8f640226
Amit Singh <amit489@gmail.com>
2024-11-11 01:25:28 +0530

    resolve 11/11

--- app/api/prompt/[id]/route.js
+++ app/api/prompt/[id]/route.js
@@ -21,13 +21,14 @@ export const PATCH = async (request, { params }) => {
     try {
         await connectToDB();
 
+        // Find the existing prompt by ID
         const existingPrompt = await Prompt.findById(params.id);
 
         if (!existingPrompt) {
             return new Response("Prompt not found", { status: 404 });
         }
 
-        
+        // Update the prompt with new data
         existingPrompt.prompt = prompt;
         existingPrompt.tag = tag;
 
@@ -43,7 +44,7 @@ export const DELETE = async (request, { params }) => {
     try {
         await connectToDB();
 
-        
+        // Find the prompt by ID and remove it
         await Prompt.findByIdAndRemove(params.id);
 
         return new Response("Prompt deleted successfully", { status: 200 });
